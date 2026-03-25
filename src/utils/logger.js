bff248da67983c620f3a0f7d6d31dbbdd2651ab0
sergey-uhanov <uhanov.sergio@gmail.com>
2026-03-25 14:10:00 +0200

    fix logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,21 +1,26 @@
-
 import pino from 'pino';
 
 
 const isProd = process.env.NODE_ENV === 'production';
 
-export const logger = pino( {
+export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    transport: {
-            target: 'pino-pretty',
-            options: {
-                colorize: true,
-                singleLine: true,
+
+    ...(isProd
+        ? {}
+        : {
+            transport: {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    singleLine: true,
+                },
             },
-        },
+        }),
+
     formatters: {
         level(label) {
-            return { level: label };
+            return {level: label};
         },
     },
 
